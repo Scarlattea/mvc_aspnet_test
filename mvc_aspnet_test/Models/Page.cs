fd0418e4466559387ff4fd2f05433b3c8c2ff134
AzureAD\ZsofiaSzekely <zsofia.szekely@prefixbox.com>
2022-08-02 17:48:07 +0200

    Page creation notification added

--- mvc_aspnet_test/Models/Page.cs
+++ mvc_aspnet_test/Models/Page.cs
@@ -5,11 +5,10 @@ namespace mvc_aspnet_test.Models
     public class Page
     {
         public int Id { get; set; }
-        [Required]
+        [Required, MinLength(2,  ErrorMessage ="Minumum length is 2")]
         public string Title { get; set; }
-        [Required]
         public string Slug { get; set; }
-        [Required]
+        [Required, MinLength(4, ErrorMessage = "Minumum length is 4")]
         public  string Content { get; set; }    
         public int Sorting { get; set; }    
     }
