b5b6d71cd6024a489fbdf48f692cfa3c69a71e41
AzureAD\ZsofiaSzekely <zsofia.szekely@prefixbox.com>
2022-09-02 15:33:40 +0200

    default view added

---
+++ mvc_aspnet_test/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc_aspnet_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_aspnet_test.Infrastructure
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly CmsShoppingCartContext context;
+        public MainMenuViewComponent(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPagesAsync()
+        {
+            return context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
